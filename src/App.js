1dc515d336d9a0170765677e8ea8fedaec3eba30
siwa kornnum <77187593+Siwakornnum@users.noreply.github.com>
2022-12-27 15:27:20 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./App.css";
+import { TextField, RadioGroup, Radio, FormControlLabel, FormLabel, FormControl } from "@mui/material";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h2 className="title">Test</h2>
+        <div className="Questionsfield">
+          <h3 className="text"> ข้อที่ 1</h3>
+          <TextField
+            helperText=""
+            id="demo-helper-text-aligned"
+            label="Questions"
+            className="textfield"
+          />
+          <FormControl className="radiofield">
+            <FormLabel id="demo-radio-buttons-group-label">Choice</FormLabel>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="ข้อท๊่ 1 "
+              name="radio-buttons-group"
+              className="tss"
+            >
+              <FormControlLabel value="ข้อท๊่ 1 " control={<Radio />} label="ข้อท๊่ 1 " />
+              <FormControlLabel
+                value="ข้อท๊่ 2"
+                control={<Radio />}
+                label="ข้อท๊่ 2"
+              />
+              <FormControlLabel
+                value="ข้อท๊่ 3"
+                control={<Radio />}
+                label="ข้อท๊่ 3"
+              />
+            </RadioGroup>
+          </FormControl>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
